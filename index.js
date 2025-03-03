9c26c2aae2d8d647e46ab6b2276b8d8c8061e468
inekruz <nekruz15ik@gmail.com>
2025-03-03 13:17:30 +0500

    add devide

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ class Calculator {
     multiply(a, b) {
         return a * b;
     }
+
+    divide(a, b) {
+        if (b === 0) {
+            throw new Error("Деление на ноль");
+        }
+        return a / b;
+    }
 }
 
 module.exports = Calculator;
