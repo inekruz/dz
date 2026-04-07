02348e6b85f8e5b4248250340e57d17c849d0ab1
inekruz <110328750+inekruz@users.noreply.github.com>
2026-04-07 08:13:15 +0500

    Update 4.php

--- 4.php
+++ 4.php
@@ -1,16 +1,21 @@
 <?php
 function isPalindrome($str) {
     $str = str_replace(' ', '', $str);
-    $str = strtolower($str);
     
-    return $str === strrev($str);
+    $str = mb_strtolower($str, 'UTF-8');
+    
+    $chars = preg_split('//u', $str, -1, PREG_SPLIT_NO_EMPTY);
+    
+    $reversedChars = array_reverse($chars);
+    
+    return $chars === $reversedChars;
 }
 
 echo "Введите строку: ";
 $input = trim(fgets(STDIN));
 
 if (empty($input)) {
-    echo "Пожалуйста, введите строку\n";
+    echo "Ошибка: Вы ничего не ввели!\n";
 } else {
     if (isPalindrome($input)) {
         echo "\"$input\" - это палиндром!\n";
